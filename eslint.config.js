acd7e137d9f8470a6efd10891ed70615cceef202
Mostafa-Zak <mostafazakzok21@gmail.com>
2025-11-14 03:04:40 +0200

    fixing some errors

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,10 @@ export default antfu({
   vue: true,
   typescript: true,
   astro: true,
+  // ADD THE 'ignores' PROPERTY HERE
+  ignores: [
+    'src/content/blog/**', // This ignores all files and subdirectories inside src/content/blog
+  ],
   formatters: {
     astro: true,
     css: true,
